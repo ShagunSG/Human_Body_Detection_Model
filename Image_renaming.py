79ce79b81738e51d6e1dd61a32d2b7406bd33447
ShagunSG <shaguneghatak@gmail.com>
2024-04-08 19:55:53 +0530

    renamed and classified in validation

--- Image_renaming.py
+++ Image_renaming.py
@@ -7,18 +7,18 @@ import csv
 
 # # Function to rename the images
 def rename_images():
-    path = "Screenshots/train/augmented_human"
+    path = "Screenshots/test/augmented_human"
     files = os.listdir(path)
-    i = 570
+    i = 2344
     for file in files:
         os.rename(os.path.join(path, file), os.path.join(path, "Screenshot_" + str(i) + ".png"))
         i += 1
 
 # Function to write the new names to a csv file
 def write_to_csv():
-    path = "Screenshots/train/augmented_human"
+    path = "Screenshots/test/augmented_human"
     files = os.listdir(path)
-    with open("Image_classification_1.csv", "w", newline="") as file:
+    with open("Image_classification_2.csv", "w", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(["New Name", "Image Classification"])
         for file in files:
